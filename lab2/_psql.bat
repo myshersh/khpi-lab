8a54b20a22974849a92737a45a841a4892ee541d
myshersh <mshershnev@gmail.com>
2023-05-28 15:53:42 +0300

    fix

--- lab2/_psql.bat
+++ lab2/_psql.bat
@@ -1,1 +1,1 @@
-docker-compose exec --env PGPASSWORD=password --interactive --tty postgres_db psql --username="school-user" --dbname="school" %*
+docker-compose exec --env PGPASSWORD=password --interactive --tty postgres_db psql --username="suser" --dbname="school" %*
--- lab2/_psql.sh
+++ lab2/_psql.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose exec --env PGPASSWORD=password --interactive --tty postgres_db psql --username="school-user" --dbname="school" $@
+docker-compose exec --env PGPASSWORD=password --interactive --tty postgres_db psql --username="suser" --dbname="school" $@
