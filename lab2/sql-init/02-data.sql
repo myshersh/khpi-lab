bf907f18734f50c84331a21c939f4e6aa7826083
myshersh <mshershnev@gmail.com>
2023-05-28 17:03:14 +0300

    fix

--- lab2/sql-init/02-data.sql
+++ lab2/sql-init/02-data.sql
@@ -1,9 +1,8 @@
     \connect school;
     SET ROLE "suser";
 
-    INSERT INTO classrooms (class_name)
+    INSERT INTO classrooms (room_number)
     VALUES (201), (202);
-
 
     INSERT INTO teachers (last_name, first_name, classroom_id)
     VALUES ('Smith', 'Viktoria', 201), ('Dorn', 'Joe', 202);
