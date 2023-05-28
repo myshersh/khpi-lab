3d3f911e2cbad5d80c287a9e88d34bffd2e50dbb
myshersh <mshershnev@gmail.com>
2023-05-28 16:29:32 +0300

    fix

--- lab2/sample_queries.sql
+++ lab2/sample_queries.sql
@@ -2,8 +2,4 @@
 SET ROLE "suser";
 
 -- скільки вчителів без кабінету
-SELECT COUNT(1) FROM teachers
-WHERE NOT EXISTS (
-    SELECT 1 FROM teachers
-    WHERE classroom_id IS NULL
-);
+SELECT * FROM teachers
