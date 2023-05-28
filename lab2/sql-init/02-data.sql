e793a4f6519184eaa13c373be41f6cc6054a2cef
myshersh <mshershnev@gmail.com>
2023-05-28 17:08:35 +0300

    fix

--- lab2/sql-init/02-data.sql
+++ lab2/sql-init/02-data.sql
@@ -4,5 +4,5 @@
     INSERT INTO classrooms (room_number)
     VALUES (201), (202);
 
-    INSERT INTO teachers (last_name, first_name, classroom_id)
+    INSERT INTO teachers (last_name, first_name, room_number)
     VALUES ('Smith', 'Viktoria', 201), ('Dorn', 'Joe', 202);
